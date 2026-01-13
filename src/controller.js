a847edabab9ede94fc889df2733dfee30ff1ac5f
adrian adewunmi <adrian.a.adewunmi@googlemail.com>
2026-01-13 17:05:36 +0000

    Implement saveTransactionInLS() function

--- src/controller.js
+++ src/controller.js
@@ -1,7 +1,17 @@
 import { Transaction, transactionType } from "./model";
 
-const temp = new Transaction(transactionType.INCOME, 4000);
-
 const getTransactionFromLS = (type)=>{
     return JSON.parse(localStorage.getItem(type) || '[]');
-}
+};
+
+const saveTransactionInLS = (transaction) => {
+    let data = getTransactionFromLS(transaction.type);
+    if (Array.isArray(data)) {
+        data.push(transaction);
+        localStorage.setItem(transaction.type, JSON.stringify(data));
+    }
+};
+
+const temp = new Transaction(transactionType.INCOME, 4000);
+saveTransactionInLS(temp)
+console.log(temp)
