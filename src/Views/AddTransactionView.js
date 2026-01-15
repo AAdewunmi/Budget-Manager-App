bd2c7cf8584385707a91dc1b1e6ef1631c50db8f
adrian adewunmi <adrian.a.adewunmi@googlemail.com>
2026-01-15 14:51:37 +0000

    Implement Add Transaction view

--- src/Views/AddTransactionView.js
+++ src/Views/AddTransactionView.js
@@ -2,7 +2,8 @@ class AddTransactionView{
     parentElement = document.querySelector(".add_transaction_form");
     valueInput = this.parentElement.querySelector(".value_input");
     constructor(){
-        this.parentElement.addEventListener("submit",()=>{
+        this.parentElement.addEventListener("submit",(event)=>{
+            event.preventDefault();
             console.log("Value is ..... ", this.valueInput.value);
         })
     }
