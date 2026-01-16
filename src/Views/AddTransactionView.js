5f33c22d996fa0298fb6824bb08449955aca87ec
adrian adewunmi <adrian.a.adewunmi@googlemail.com>
2026-01-16 12:03:37 +0000

    Implement addSubmitHandler() function

--- src/Views/AddTransactionView.js
+++ src/Views/AddTransactionView.js
@@ -1,11 +1,15 @@
 class AddTransactionView{
     parentElement = document.querySelector(".add_transaction_form");
     valueInput = this.parentElement.querySelector(".value_input");
+    typeSelect = this.parentElement.querySelector(".transaction_type");
     constructor(){
-        this.parentElement.addEventListener("submit",(event)=>{
-            event.preventDefault();
-            console.log("Value is ..... ", this.valueInput.value);
-        })
+        this.typeSelect.addEventListener("change", ()=>{
+            console.log("Type changed to ", this.typeSelect.value);
+        });
+    }
+
+    addSubmitHandler(handler){
+        this.parentElement.addEventListener("submit", handler.bind(this));
     }
 }
 
