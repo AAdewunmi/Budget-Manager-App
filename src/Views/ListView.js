046d137fddf8275152d4d8c5c738b14dd6a1a326
adrian adewunmi <adrian.a.adewunmi@googlemail.com>
2026-02-14 05:31:55 +0000

    refactor(list-view): improve timestamp formatting in ListView

--- src/Views/ListView.js
+++ src/Views/ListView.js
@@ -1,32 +1,37 @@
-export class ListView{
-    render(data){
-        this.data = data;
-        const html = this.generateHTMLString(); 
-        this.container.innerHTML = html;
-    }
+export class ListView {
+  render(data) {
+    this.data = data;
+    const html = this.generateHTMLString();
+    this.container.innerHTML = html;
+  }
 
-    pushTransitionInContainer(transaction){
-        this.container.insertAdjacentHTML(
-            "afterbegin", 
-            this.generateCardHTML(transaction)
-        );
-    }
+  pushTransitionInContainer(transaction) {
+    this.container.insertAdjacentHTML(
+      "afterbegin",
+      this.generateCardHTML(transaction),
+    );
+  }
 
-    generateCardHTML(transaction){
-        const description = (transaction.description || "").trim() || "No description";
-        return `<div class="transaction_card">
+  generateCardHTML(transaction) {
+    const description =
+      (transaction.description || "").trim() || "No description";
+    return `<div class="transaction_card">
         <div>${description} - ${transaction.value} - ${transaction.timestamp}</div>
         </div>`;
-    }
+  }
 
-    generateHTMLString(){
-        const data = this.data;
-        let html = "";
-        if(Array.isArray(data)){
-            data.forEach(transaction=>{
-                html += this.generateCardHTML(transaction); 
-            })
-        }
-        return html;
+  formatTimestamp(timestamp) {
+    return new Date(timestamp).toDateString();
+  }
+
+  generateHTMLString() {
+    const data = this.data;
+    let html = "";
+    if (Array.isArray(data)) {
+      data.forEach((transaction) => {
+        html += this.generateCardHTML(transaction);
+      });
     }
+    return html;
+  }
 }
